cd1ffb78a27da0520925e9d3f789e0057a752f17
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-25 12:12:27 +0600

    php5

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $name = 'Md Zia';
+    $name .= ' Uddin';
+    
+    echo $name;
+    echo '<br/>';
+    
+    var_dump(10 > 10);
+    echo '<br/>';
+    var_dump1(10 < 10); 
+    echo '<br/>';
+    var_dump2(10 == 10); 
+    var_dump3(10 != 10); 
+    var_dump4(10 == 10); 
+    var_dump5(10 === 10); 
+    var_dump6('10' === 10); 
+    var_dump8(10 >= 10); 
+    var_dump9(10 <= 10); 
+ 
+?>
